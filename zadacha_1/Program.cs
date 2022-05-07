f0dbe01c6f43cb2e75261b5399ac36c1b074ebf4
sergey.k <k.sergey2010@yandex.ru>
2022-05-07 14:10:33 +0700

    done

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,33 @@
+﻿void Vvod(int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());   
+    }
+}
+
+void Print(int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");   
+    }
+    Console.WriteLine();
+}
+
+int Polojit(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+        result++;
+    }
+    return result;
+}
+
+int M = 5;
+int[] array = new int[M];
+Vvod(array);
+Print(array);
+System.Console.WriteLine(Polojit(array));
---
+++ zadacha_2/Program.cs
@@ -0,0 +1,13 @@
+﻿void Peresechenie(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * x) + b1;
+    Console.WriteLine(x + " ");
+    Console.WriteLine(y);
+}
+
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+Peresechenie(b1, k1, b2, k2);
